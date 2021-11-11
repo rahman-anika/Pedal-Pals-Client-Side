8a2720cdca648cf115bb4b3f679b6438267c85e9
rahman-anika <anikarahman37@gmail.com>
2021-11-11 21:45:40 +0600

    pay modified

--- src/components/Dashboard/Pay/Pay.js
+++ src/components/Dashboard/Pay/Pay.js
@@ -4,12 +4,12 @@ import payment from './../../../images/dashboard/payment.jpg';
 const Pay = () => {
     return (
         <div>
-            <div>
-                <h2 className="mt-5">Payment System is Coming Soon!!</h2>
 
-                <img src={payment} alt="" />
+            <h2 className="mt-5">Payment System is Coming Soon!!</h2>
 
-            </div>
+            <img src={payment} alt="" />
+
+
 
         </div>
     );
